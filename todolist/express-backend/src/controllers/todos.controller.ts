4814783627b7ebe84f7de6870e807c57016d79a4
Novpa <aa.novparadiputra@binus.ac.id>
2026-03-04 11:36:18 +0700

    exercise resapi done

--- todolist/express-backend/src/controllers/todos.controller.ts
+++ todolist/express-backend/src/controllers/todos.controller.ts
@@ -3,7 +3,120 @@ import pool from "../config/pool-connection.config";
 
 export const todosController = {
   async getTodos(req: Request, res: Response) {
-    const result = await pool.query("select * from todos");
-    res.json(result.rows);
+    try {
+      const result = await pool.query(
+        "select * from todos where deletedon is null",
+      );
+      return res.status(200).json({
+        status: "successful",
+        data: result.rows,
+      });
+    } catch (error) {
+      console.error(error);
+      return res.status(500).json({ messsage: "Server error" });
+    }
+  },
+
+  async getAllTodosById(req: Request, res: Response) {
+    const id = Number(req.params.id);
+
+    try {
+      if (isNaN(id)) {
+        return res.status(400).json({ message: "id is required and a number" });
+      }
+      const result = await pool.query("select * from todos where id = $1", [
+        id,
+      ]);
+
+      if (!result.rows.length) {
+        return res.status(400).json({ message: "id is unknown" });
+      }
+
+      return res.status(200).json({
+        status: "successful",
+        data: result.rows,
+      });
+    } catch (error) {
+      console.error(error);
+      return res.status(500).json({ messsage: "Server error" });
+    }
+  },
+
+  async createTodos(req: Request, res: Response) {
+    const { task } = req.body;
+
+    try {
+      if (!task) {
+        return res.status(400).json({ message: "task is required" });
+      }
+
+      const result = await pool.query(
+        "insert into todos (task) values ($1) returning *",
+        [task],
+      );
+
+      return res.status(201).json({
+        status: "successful",
+        data: result.rows,
+      });
+    } catch (error) {
+      console.error(error);
+      return res.status(500).json({ messsage: "Server error" });
+    }
+  },
+
+  async softDelete(req: Request, res: Response) {
+    const id = Number(req.params.id);
+    const currentDate = new Date().toISOString();
+    try {
+      if (isNaN(id)) {
+        return res.status(400).json({ message: "id is required and a number" });
+      }
+
+      const result = await pool.query(
+        "update todos set deletedon = $1 where id = $2 returning *",
+        [currentDate, id],
+      );
+
+      if (!result.rows.length) {
+        return res.status(400).json({ message: "id is unknown" });
+      }
+
+      return res.status(201).json({
+        status: "successful",
+        message: "delete successful",
+        data: result.rows,
+      });
+    } catch (error) {
+      console.error(error);
+      return res.status(500).json({ messsage: "Server error" });
+    }
+  },
+
+  async updateTodos(req: Request, res: Response) {
+    const task = req.body.task;
+    const id = Number(req.params.id);
+    const currentDate = new Date().toISOString();
+    try {
+      if (isNaN(id)) {
+        return res.status(400).json({ message: "id must a number" });
+      }
+      if (!task) {
+        return res.status(400).json({ message: "new task is empty" });
+      }
+      const result = await pool.query(
+        "update todos set task = $1, updatedon = $2 where id = $3 returning *",
+        [task, currentDate, id],
+      );
+
+      return res.status(201).json({
+        status: "successful",
+        message: "update successful",
+        data: result.rows,
+      });
+    } catch (error) {
+      console.error(error);
+      return res.status(500).json({ messsage: "Server error" });
+    }
   },
 };
--- todolist/express-backend/src/routes/todos.routes.ts
+++ todolist/express-backend/src/routes/todos.routes.ts
@@ -4,5 +4,9 @@ import { todosController } from "../controllers/todos.controller";
 const router = Router();
 
 router.get("/", todosController.getTodos);
+router.get("/:id", todosController.getAllTodosById);
+router.post("/", todosController.createTodos);
+router.post("/:id", todosController.softDelete);
+router.put("/:id", todosController.updateTodos);
 
 export default router;
